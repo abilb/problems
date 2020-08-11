3fb763a546fb9e4281dd440f805f449aa983fb64
Abilb <info@abilb.com>
2020-08-11 16:54:12 -0700

    Find Prime factors of given number

---
+++ src/main/java/abilb/problems/math/PrimeFactors.java
@@ -0,0 +1,67 @@
+package abilb.problems.math;
+
+import java.util.*;
+import abilb.problems.abstraction.Prime;
+
+public class PrimeFactors {
+  Prime _prime;
+
+  public PrimeFactors(){
+    _prime = new PrimeImpl();
+  }
+
+  public List<Integer> solveOptimal( int n ){
+    if( n < 1 || n > 1000000 )
+      throw new IllegalArgumentException("Value must be greate than zero or less than 10000");
+    
+    List<Integer> list = new ArrayList<Integer>();
+
+    if( n == 2 || n==3 )
+      return list;
+
+    while( n%2==0 ){
+      list.add(2);
+      n /= 2;
+    }
+
+    while( n%3==0 ){
+      list.add(3);
+      n /= 3;
+    }
+
+    for( int i=5; i*i <= n; i=i+6 ){
+      while( n%i==0 ){
+        list.add(i);
+        n = n/i;
+      }
+      while( n%(i+2) == 0 ){
+        list.add(i+2);
+        n = n/(i+2);
+      }
+    }
+
+    if(n>1)
+      list.add(n);
+
+    return list;
+  }
+
+  public List<Integer> solveNaive(int n){
+    if( n < 1 || n > 1000000 )
+      throw new IllegalArgumentException("Value must be greater than zero and less than 10000");    
+
+    List<Integer> factors = new ArrayList<Integer>();
+
+    for( int i = 2; i <= n/2; i++ )
+      if( _prime.check(i) ){
+        int x = i;
+        while( n%x==0 ){
+          factors.add(i);
+          x = x*i;
+        }
+      }
+
+    return factors;
+  }
+
+}
---
+++ src/test/java/abilb/problems/math/PrimeFactorsTest.java
@@ -0,0 +1,126 @@
+package abilb.problems.math;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.*;
+
+public class PrimeFactorsTest {
+  private PrimeFactors _primeFactors;
+
+   public PrimeFactorsTest(){
+    _primeFactors = new PrimeFactors();
+  }
+
+  @Test
+  public void testDifferentCasesOptimal(){
+    int[] nums = new int[10];
+    Integer[][] factors = new Integer[10][];
+
+    nums[0] = 100;
+    factors[0] = new Integer[]{2,2,5,5};
+
+    nums[1] = 12;
+    factors[1] = new Integer[]{2,2,3};
+
+    nums[2] = 450;
+    factors[2] = new Integer[]{2,3,3,5,5};
+
+    nums[3] = 1;
+    factors[3] = new Integer[0];
+
+    nums[4] = 13;
+    factors[4] = new Integer[]{13};
+
+    nums[5] = 84;
+    factors[5] = new Integer[]{2,2,3,7};
+
+    nums[6] = 9085;
+    factors[6] = new Integer[]{5,23,79};
+
+    nums[7] = 118105;
+    factors[7] = new Integer[]{5,13,23,79};
+
+    nums[8] = 3;
+    factors[8] = new Integer[0];
+
+    for(int i = 0; i < nums.length; i++ ){
+
+      if( factors[i] == null )
+        break;
+
+      int num = nums[i];
+      
+      Arrays.sort( factors[0] );
+      List<Integer> expected = Arrays.asList(factors[i]);
+
+      List<Integer> actual = _primeFactors.solveOptimal(num);
+      Collections.sort(actual);
+
+      assertEquals(expected,actual,"solveOptimal("+num+")");
+    }
+  }
+
+  @Test
+  public void testDifferentCasesNaive(){
+    int[] nums = new int[10];
+    Integer[][] factors = new Integer[10][];
+
+    nums[0] = 100;
+    factors[0] = new Integer[]{2,2,5,5};
+
+    nums[1] = 12;
+    factors[1] = new Integer[]{2,2,3};
+
+    nums[2] = 450;
+    factors[2] = new Integer[]{2,3,3,5,5};
+
+    nums[3] = 1;
+    factors[3] = new Integer[0];
+
+    nums[4] = 13;
+    factors[4] = new Integer[0];
+
+    nums[5] = 84;
+    factors[5] = new Integer[]{2,2,3,7};
+
+    nums[7] = 118105;
+    factors[7] = new Integer[]{5,13,23,79};
+
+    nums[7] = 118105;
+    factors[7] = new Integer[]{5,13,23,79};
+
+    nums[8] = 3;
+    factors[8] = new Integer[0];
+
+    for(int i = 0; i < nums.length; i++ ){
+
+      if( factors[i] == null )
+        break;
+
+      int num = nums[i];
+      
+      Arrays.sort( factors[0] );
+      List<Integer> expected = Arrays.asList(factors[i]);
+
+      List<Integer> actual = _primeFactors.solveNaive(num);
+      Collections.sort(actual);
+
+      assertEquals(expected,actual,"solveNaive("+num+")");
+    }
+  }
+
+  @Test
+  public void testExceptions(){
+    Exception exception = assertThrows( IllegalArgumentException.class, () -> {
+      _primeFactors.solveNaive(0);
+    });
+
+    String expectedMessage = "greater than zero";
+    String actualMessage = exception.getMessage();
+
+    assertTrue(actualMessage.contains(expectedMessage));
+  }
+}
