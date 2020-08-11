4e32542dbb0b42c2c24a1f02de2014e130da5752
Abilb <info@abilb.com>
2020-08-10 18:22:51 -0700

    Trailing Zeros

---
+++ src/main/java/abilb/problems/math/TrailingZeros.java
@@ -0,0 +1,19 @@
+package abilb.problems.math;
+
+public class TrailingZeros {
+  public int count(int n){
+
+    if( n < 0 )
+      throw new IllegalArgumentException("Value must be greater than zero");
+
+    int sumOfZeros = 0;
+    int i = 5;
+
+    while( i < n ){
+      sumOfZeros += n/i;
+      i *= 5;
+    }
+
+    return sumOfZeros;
+  }
+}
---
+++ src/test/java/abilb/problems/math/TrailingZerosTest.java
@@ -0,0 +1,50 @@
+package abilb.problems.math;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TrailingZerosTest {
+  private TrailingZeros _trailingZeros;
+
+  public TrailingZerosTest(){
+    _trailingZeros = new TrailingZeros();
+  }
+
+  @Test 
+  public void testDifferentCases(){
+    int[][] cases = new int[10][];
+    
+    cases[0] = new int[]{10, 2};
+    cases[1] = new int[]{13, 2};
+    cases[2] = new int[]{15, 3};
+    cases[3] = new int[]{50, 12};
+    cases[4] = new int[]{2, 0};
+    cases[5] = new int[]{126, 31};
+    cases[6] = new int[]{149, 35};
+
+    for( int[] cs: cases ){
+      if(cs==null)
+        break;
+
+      int expected = cs[1];
+      int n = cs[0];      
+
+      int actual = _trailingZeros.count(n);
+      assertEquals(expected,actual,"count("+n+")");
+    }
+  }
+
+  @Test 
+  public void testZeroValue(){
+    Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+      _trailingZeros.count(-9);
+    });
+
+    String expectedMessage = "Value must be greater than zero";
+    String actualMessage = exception.getMessage();
+
+    assertTrue(actualMessage.contains(expectedMessage));
+  }
+}
