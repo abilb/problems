33e7d3b848877819d9139e0cb4cca8eee8b106c2
Abilb <info@abilb.com>
2020-08-22 08:31:42 -0700

    Array: solution for problem "Trapping Rain Water"

---
+++ src/test/java/abilb/problems/array/TrappingRainWaterTest.java
@@ -0,0 +1,40 @@
+package abilb.problems.array;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+
+public class TrappingRainWaterTest {
+
+  @Test
+  public void testVariousCases(  ){
+    TrappingRainWater obj = new TrappingRainWater();
+
+    int[] expects = new int[]{6,2,7,0,0};
+
+    int[][] cases = new int[10][];
+    cases[0] = new int[]{0,1,0,2,1,0,1,3,2,1,2,1};
+    cases[1] = new int[]{2,0,2};
+    cases[2] = new int[]{3, 0, 2, 0, 4};
+    cases[3] = new int[]{1,2,3,4,5,6,7};
+    cases[4] = new int[]{5,4,3,2,1};
+
+    for( int i = 0; i < cases.length; i++ ){
+      if( cases[i]==null )
+        break;
+
+      int expected = expects[i];
+
+      int[] input = cases[i];
+      int actual = obj.find(input);
+
+      assertEquals( expected, actual, "find(" + Arrays.toString(input)+") - expected:" + expected + ", actual: "+actual );
+    }
+  }
+  
+}
