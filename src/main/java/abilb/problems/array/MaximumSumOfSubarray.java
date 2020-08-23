93aef8ed63517209f4135ada637455096161e8a5
Abilb <info@abilb.com>
2020-08-22 21:34:42 -0700

    Array: Maximum sum of Subarray (solution+tests)

---
+++ src/main/java/abilb/problems/array/MaximumSumOfSubarray.java
@@ -0,0 +1,42 @@
+package abilb.problems.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MaximumSumOfSubarray {
+  public int find(int[] arr){
+    return optimal(arr);
+  }
+
+  // time complexity O(n)
+  private int optimal( int[] arr ){
+    int n = arr.length;    
+    int prevSum = arr[0];
+    int maxSum = prevSum;
+
+    for( int i=1; i < n; i++ ){
+      int currSum = prevSum + arr[i];
+      prevSum = Math.max( currSum, arr[i] );
+      maxSum = Math.max( maxSum, prevSum );
+    }
+    
+    return maxSum;
+  }
+
+  // time complexity O(n^2)
+  private int naive(int[] arr){
+    int n = arr.length;
+    int maxSum = Integer.MIN_VALUE;  
+    int localSum = 0;
+    
+    for( int i=0; i < n; i++ )  {
+      localSum = 0;
+      for( int j = i; j < n; j++ ){
+        localSum += arr[j];
+        maxSum = Math.max( maxSum, localSum );
+      }
+    }
+
+    return maxSum;
+  }
+}
