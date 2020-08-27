ccca78cc3d5060694148e6d6f727f35313e795e3
Abilb <info@abilb.com>
2020-08-26 20:53:23 -0700

    Arrays: Prefix Sum Array (Solution+Tests)

---
+++ src/main/java/abilb/problems/array/PrefixSumArray.java
@@ -0,0 +1,23 @@
+package abilb.problems.array;
+
+import java.util.Arrays;
+
+public class PrefixSumArray {
+  public int find(int[] arr, int b, int e){
+    return calculate(arr, b, e);
+  }
+
+  private int calculate( int[] arr, int b, int e ){
+    int n = arr.length;
+    int[] sumTable = new int[n];
+    sumTable[0] = arr[0];
+
+    for(int i=1; i<n; i++)
+      sumTable[i] += arr[i] + sumTable[i-1];
+    
+    if( b==0 )
+      return sumTable[e];
+
+    return sumTable[e] - sumTable[b-1];
+  }
+}
---
+++ src/test/java/abilb/problems/array/PrefixSumArrayTest.java
@@ -0,0 +1,59 @@
+package abilb.problems.array;
+
+import java.util.*;
+import abilb.problems.Utils;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PrefixSumArrayTest {  
+
+  @Test
+  public void testVariousCases(){
+    PrefixSumArray obj = new PrefixSumArray();
+        
+    int[][] cases   = new int[10][];    
+    int[][] queries = new int[10][];
+    int[] expects   = new int[10];
+
+    cases[0] = new int[]{2,8,3,9,6,5,4};
+    
+    queries[0] = new int[]{0,2};    
+    expects[0] = 13;
+    
+    queries[1] = new int[]{1,3};    
+    expects[1] = 20;
+    
+    queries[2] = new int[]{2,6};    
+    expects[2] = 27;
+    
+    queries[3] = new int[]{6,6};    
+    expects[3] = 27;
+    
+
+    for( int i = 0; i < queries.length; i++ ){
+      
+      if( cases[i] == null )
+        break;
+
+      int expect = expects[i];
+      int[] input = cases[0];
+      int b = queries[i][0];
+      int e = queries[i][1];
+
+      int actual = obj.find(input, b, e);
+
+      assertEquals( expect, actual, "find("+ Arrays.toString(input)+") - expected:" + expect + ", actual: "+actual );
+   }
+
+  }
+
+}
